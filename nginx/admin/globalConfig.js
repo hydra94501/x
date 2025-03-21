71d9c55ca6b56ddd4542af13f5bbe6b03a58796b
hydra94501 <hydra94501@gmail.com>
2025-03-21 16:09:56 +0800

    1
    1

---
+++ nginx/admin/globalConfig.js
@@ -0,0 +1,6 @@
+globalConfig = { // 兼容ie
+  // logo_path : '@/assets/top1.png',
+  logo_path : '@/assets/latincopay.jpg',
+  http_base_path : 'http://127.0.0.1:8083'
+  // http_base_path : 'https://tspay.online'
+}
